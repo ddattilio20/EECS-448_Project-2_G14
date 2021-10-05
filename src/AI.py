9d1278c75d27cb66c72569766edbc6485779fda0
gsburmaster <gsburmaster@gmail.com>
2021-10-05 16:51:20 -0500

    continued messing with medium shot

--- src/AI.py
+++ src/AI.py
@@ -10,9 +10,10 @@ class AI:
     #Variable for tracking where the hard AI is at to know the next space to shoot at
     hardPlace = 0
 
-    #prevShot is a variable for storing if the medium AI previously hit a shot or not
-    prevShot = False
-    prevShotPos = None
+    #prevShotInfo is a variable for storing if the medium AI previously hit a shot or not, the position of the previous shot if there is one, and where in recursion we are for tracking the shot (Left, Up, Right, or Down)
+    # includes parameters [whether "last shot" was a hit (bool)    ,  position of last shot    ,  Which direction we need to check , First shot that was a hit   ]
+    # last shot isn't always the most recent shot, but rather whether or not the firing from adjacent squares portion of the code needs to run. you could miss a shot and still be in last shot hit mode.
+    prevShotInfo = [False, None, "Left", None]
     aiOpp = 'N'
 
 
@@ -52,14 +53,17 @@ class AI:
         colTarget = None
         rowTarget = None
         #if the previous shot didn't hit, then shoot randomly
-        if prevShot is False:    
+        if prevShot[0] is False:    
             colTarget = colArr[colRand]
             rowTarget = random.randrange(1,9,1)
             int_Col = ord(colTarget) - 64
             hitOrMiss = opponentBoard.shotOn(rowTarget - 1, int_Col - 1)
             results = [rowTarget, colTarget, hitOrMiss]
             return(results)
-        
+        if prevShot[0] is True:
+            if 
+
+            return
 
         return
         
