1fd71c9ca41ab405597a7a14d135912bdc024013
gsburmaster <gsburmaster@gmail.com>
2021-10-05 23:26:30 -0500

    resolved conflicts with code, continued work on medfire

--- src/AI.py
+++ src/AI.py
@@ -14,7 +14,7 @@ class AI:
     # includes parameters [whether "last shot" was a hit (bool)    ,  position of last shot    ,  Which direction we need to check , First shot that was a hit   ]
     # last shot isn't always the most recent shot, but rather whether or not the firing from adjacent squares portion of the code needs to run. you could miss a shot and still be in last shot hit mode.
     prevShotInfo = [False, None, "Left", None]
-    aiOpp = False
+aiOpp = False
 
 
     #Method for easy difficulty AI
@@ -45,7 +45,8 @@ class AI:
 
     #Shots previous are not taken into account
     #still need to add previous shot detection
-    def mediumFire(opponentBoard, prevShot):
+    #functionality is supposed to be semi smart (shoot straight once you get a 2 streak)
+    def mediumFire(opponentBoard, prevShotInfo):
         #Array of possible column inputs for firing at
         colArr = ['A','B','C','D','E','F','G','H','I','J']
         # Generates a random number between 0-9 to index the array
@@ -53,16 +54,22 @@ class AI:
         colTarget = None
         rowTarget = None
         #if the previous shot didn't hit, then shoot randomly
-        if prevShot[0] is False:    
+        if prevShotInfo[0] is False:    
             colTarget = colArr[colRand]
             rowTarget = random.randrange(1,9,1)
             int_Col = ord(colTarget) - 64
             hitOrMiss = opponentBoard.shotOn(rowTarget - 1, int_Col - 1)
+            if  (hitOrMiss != 0):
+                prevShotInfo[0] = True
+                prevShotInfo[1] = (rowTarget-1,int_Col-1)
+                prevShotInfo[3] = (rowTarget-1,int_Col-1)
             results = [rowTarget, colTarget, hitOrMiss]
-            return(results)
-        if prevShot[0] is True:
-           # if 
 
+            return(results)
+        if prevShotInfo[0] is True:
+           
+
+                return
             return
 
         return
