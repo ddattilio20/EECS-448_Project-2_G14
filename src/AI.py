7e28dec73d9751f512bb3d49dc9faf6a46922dd2
Logan Schmalz <loganschmalz@gmail.com>
2021-10-09 23:04:45 -0500

    Document mediumAI method

--- src/AI.py
+++ src/AI.py
@@ -59,6 +59,11 @@ class AI:
 
 
     def mediumAI(self, opponentBoard):
+        """
+        Method for medium AI.
+        Goes through a basic algorithm when a shot is hit to check each direction for another ship coordinate, starting with left.
+        Returns coordinates for shot.
+        """
         colTarget = None
         rowTarget = None
         validRowAI = [0, 1, 2, 3, 4, 5, 6, 7, 8]
